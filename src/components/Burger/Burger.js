60d4f15c4381503fe3aea94f8ee3e19cae5d658c
banecikic <branimir.cikic@gmail.com>
2020-12-06 18:18:36 +0100

    add axios and routing sun

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import { withRouter } from 'react-router-dom';
 
 
 const burger = (props) => {
-    let transformedIngredients = Object.keys(props.ingredients)
+  console.log(props);
+
+  let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
            return [...Array(props.ingredients[igKey])].map((_, i) => {
               return <BurgerIngredient type={igKey} key={igKey + i}/>;
@@ -24,4 +27,4 @@ const burger = (props) => {
     );
 };
 
-export default burger;
+export default withRouter(burger);
