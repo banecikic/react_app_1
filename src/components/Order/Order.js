1bef0922c13927e0d654bb8c62890f464ef6838b
banecikic <branimir.cikic@gmail.com>
2020-12-07 17:01:41 +0100

    minor code styling

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,10 +1,8 @@
 import React from 'react';
-
 import classes from './Order.css';
 
 const order = (props)=> {
   const ingredients = [];
-
   for(let ingredientName in props.ingredients) {
     ingredients.push({
       name: ingredientName,
@@ -29,6 +27,5 @@ const order = (props)=> {
     </div>
   );
 }
-
 
 export default order;
