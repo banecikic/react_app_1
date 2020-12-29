3214a28d4d1ad4fe1a3e3e977e1cbe75a2b624e0
Branimir Cikic <branimir.cikic@gmail.com>
2020-12-29 14:56:53 +0100

    Initial auth work (#10)
    * initial auth work
    
    * add signu and signin logic
    
    * authenticate improvements
    
    * final work to auth

--- src/App.js
+++ src/App.js
@@ -1,20 +1,50 @@
 import React, {Component} from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from "./containers/Checkout/Checkout";
 import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth';
+import Logout from './containers/Auth/Logout/Logout';
+// import {dispatch} from "jest-circus/build/state";
+import * as actions from './store/actions/index';
 
 class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  }
+
   render() {
+    let routes = (
+     <Switch>
+       <Route path="/auth" component={Auth} />
+       <Route path="/" component={BurgerBuilder} />
+     </Switch>
+    );
+
+    if (this.props.isAutheticated){
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/auth" component={Auth} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/" component={BurgerBuilder} />
+        </Switch>
+      );
+    }
     return (
         <div>
           <Layout>
             <Switch>
               <Route path="/checkout" component={Checkout} />
               <Route path="/orders" component={Orders} />
+              <Route path="/auth" component={Auth} />
+              <Route path="/logout" component={Logout} />
               <Route path="/" component={BurgerBuilder} />
+              <Redirect to="/" />
             </Switch>
           </Layout>
         </div>
@@ -22,4 +52,16 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  };
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -35,7 +35,12 @@ class BurgerBuilder extends Component {
     }
 
     purchaseHandler =  () => {
-        this.setState({purchasing: true})
+        if (this.props.isAutheticated){
+          this.setState({purchasing: true})
+        } else {
+            this.props.onSetAuthRedirectPath('/checkout');
+            this.props.history.push('/auth');
+        }
     }
 
     purchaseCancelHandler = () => {
@@ -67,6 +72,7 @@ class BurgerBuilder extends Component {
                 price={this.props.price}
                 purchasable={this.updatePurchaseState(this.props.ings)}
                 ordered={this.purchaseHandler}
+                isAuth={this.props.isAutheticated}
               />
             </Aux>
           );
@@ -93,7 +99,8 @@ const mapStateToProps = state => {
     return {
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
-        error: state.burgerBuilder.error
+        error: state.burgerBuilder.error,
+        isAutheticated: state.auth.token !== null
     };
 }
 
@@ -102,7 +109,8 @@ const mapDispatchToProps = dispatch => {
         onIngredientAdded: (ingName) => dispatch(actions.addIngredient(ingName)),
         onIngredientRemoved: (ingName) => dispatch(actions.removeIngredient(ingName)),
         onInitIngredients: () => dispatch(actions.initIngredients()),
-        onInitPurchase: () => dispatch(actions.purchaseInit())
+        onInitPurchase: () => dispatch(actions.purchaseInit()),
+        onSetAuthRedirectPath: (path) => dispatch(actions.setAuthRedirectPath(path))
     }
 }
 
